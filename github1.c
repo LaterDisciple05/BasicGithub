26e8a18e38ea6efee653fa53b5ffa614bbcd97b6
LaterDiciple05 <akshatshah11111@gmail.com>
2024-09-28 21:45:24 +0530

    To understand branch in Git hub change1 has been made as a child branch

--- github1.c
+++ github1.c
@@ -4,5 +4,7 @@ int main(int argc, char const *argv[])
     printf("My name is akshat shah, and i am excited to learn git and github concepts %c",2);
     printf("Namaste Welcome to India");
     printf("I love my parents");
+    printf("My mother's name is kajalben shah");
+    printf("My father's name is pankajbhai shah");
     return 0;
 }
