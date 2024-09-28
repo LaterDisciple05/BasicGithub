43e72084a3b8c43640237a1ff44d4f7fad934d1f
LaterDiciple05 <akshatshah11111@gmail.com>
2024-09-28 19:45:23 +0530

    Modified Intro of git hub use

--- github1.c
+++ github1.c
@@ -2,5 +2,6 @@
 int main(int argc, char const *argv[])
 {
     printf("My name is akshat shah, and i am excited to learn git and github concepts %c",2);
+    printf("Namaste Welcome to India");
     return 0;
 }
