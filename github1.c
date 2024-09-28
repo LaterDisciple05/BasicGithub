d6535c96514f1c14c08e29f732cde868dc314316
LaterDiciple05 <akshatshah11111@gmail.com>
2024-09-28 20:12:16 +0530

    Direct Commited

--- github1.c
+++ github1.c
@@ -3,5 +3,6 @@ int main(int argc, char const *argv[])
 {
     printf("My name is akshat shah, and i am excited to learn git and github concepts %c",2);
     printf("Namaste Welcome to India");
+    printf("I love my parents");
     return 0;
 }
